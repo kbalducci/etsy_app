14518642fc32350ba50df5114eb450095c6ca370
Kate <kbalducci@hotmail.com>
2015-01-20 17:28:19 -0600

    initial commit

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+class SearchController < ApplicationController
+  def index
+    query = params[:query]
+    url = "https://openapi.etsy.com/v2/listings/active?api_key=21emm2wiz86xu3vg2yspnuex&status=#{query}"
+    response = JSON.parse(open(url).read)
+    @items = response["results"]
+  end
+end
